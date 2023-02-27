871a4895dd5cd24ed7e5d7327da8b7907652ef90
Nick Emmerson <nickemmerson619@hotmail.com>
2023-02-27 21:55:08 +0000

    Add tests for string extensions

---
+++ sql-helper/SqlHelper.Test/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using SqlHelper.Extensions;
+using FluentAssertions;
+
+namespace SqlHelper.Test.Extensions
+{
+    public class StringExtensionsTests
+    {
+        [Theory]
+        [InlineData("ANGRY STRING", "angry string")]
+        [InlineData(@"
+            string with    lots        of  unnecessary
+            white space", "string with lots of unnecessary white space")]
+        [InlineData("     string with lEadinG and TRAILING white space", "string with leading and trailing white space")]
+        public void Clean_ShouldCleanInputStrings(string input, string expected)
+        {
+            // ARRANGE / ACT
+            var actual = input.Clean();
+
+            // ASSERT
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
--- sql-helper/SqlHelper/Extensions/StringExtensions.cs
+++ sql-helper/SqlHelper/Extensions/StringExtensions.cs
@@ -6,6 +6,7 @@ namespace SqlHelper.Extensions
     {
         public static string Clean(this string input)
         {
+            if (input is null) throw new ArgumentNullException(nameof(input));
             var input_transformed = input.Trim().ToLowerInvariant();
             var rgx_whitespace = new Regex("\\s+");
             return rgx_whitespace.Replace(input_transformed, " ");
