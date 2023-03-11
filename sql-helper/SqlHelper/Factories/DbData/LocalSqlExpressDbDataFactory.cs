575cd09d9c06d0d6a243bd4e16fbaa6b8a1284cf
Nick Emmerson <nickemmerson619@hotmail.com>
2023-03-11 19:18:03 +0000

    Re-write LocalSqlExpressDbDataFactory as simple wrapper

--- sql-helper/SqlHelper/Factories/DbData/LocalSqlExpressDbDataFactory.cs
+++ sql-helper/SqlHelper/Factories/DbData/LocalSqlExpressDbDataFactory.cs
@@ -1,6 +1,4 @@
 ﻿using SqlHelper.Helpers;
-using SqlHelper.Models;
-using System.Data;
 
 namespace SqlHelper.Factories.DbData
 {
@@ -30,108 +28,13 @@ namespace SqlHelper.Factories.DbData
 
         public Models.DbData Create()
         {
-            var tables = new SortedDictionary<long, Table>();
-            var columns = new SortedDictionary<(long TableId, long ColumnId), Column>();
-            var constraints = new SortedDictionary<long, Models.Constraint>();
+            var internalFactory = new ConnectionStringDbDataFactory(
+                _connectionString,
+                _queryFactory,
+                _connectionFactory,
+                _commandFactory);
 
-            using var conn = _connectionFactory.Create();
-            conn.ConnectionString = _connectionString;
-            conn.Open();
-
-            // Tables
-            using (IDbCommand command = _commandFactory.Create())
-            {
-                command.Connection = conn;
-                command.CommandText = _queryFactory.GetQueryTables();
-                using var reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    var id = Convert.ToInt64(reader["Id"]);
-                    var schema = reader["Schema"].ToString();
-                    var name = reader["Name"].ToString();
-
-                    tables.Add(id, new Table
-                    {
-                        Id = id,
-                        Schema = schema,
-                        Name = name,
-                    });
-                }
-            }
-
-            // Columns
-            using (IDbCommand command = _commandFactory.Create())
-            {
-                command.Connection = conn;
-                command.CommandText = _queryFactory.GetQueryColumns();
-                using var reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    var tableId = Convert.ToInt64(reader["TableId"]);
-                    var columnId = Convert.ToInt64(reader["ColumnId"]);
-                    var name = reader["Name"].ToString();
-                    var type = reader["Type"].ToString();
-                    var nullable = Convert.ToBoolean(reader["Nullable"]);
-
-                    columns.Add((tableId, columnId), new Column
-                    {
-                        TableId = tableId,
-                        ColumnId = columnId,
-                        Name = name,
-                        Type = type,
-                        Nullable = nullable,
-                    });
-                }
-            }
-
-            // Constraints
-            using (IDbCommand command = _commandFactory.Create())
-            {
-                command.Connection = conn;
-                command.CommandText = _queryFactory.GetQueryConstraints();
-                using var reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    var id = Convert.ToInt64(reader["Id"]);
-                    var targetTableId = Convert.ToInt64(reader["TargetTableId"]);
-                    var sourceTableId = Convert.ToInt64(reader["SourceTableId"]);
-                    var targetColumn = Convert.ToInt32(reader["TargetColumn"]);
-                    var sourceColumn = Convert.ToInt32(reader["SourceColumn"]);
-
-                    if (constraints.TryGetValue(id, out var constraint))
-                    {
-                        constraint.Columns.Add(new Models.ConstraintColumnPair
-                        {
-                            TargetColumnId = targetColumn,
-                            SourceColumnId = sourceColumn,
-                        });
-                    }
-                    else
-                    {
-                        constraints.Add(id, new Models.Constraint
-                        {
-                            Id = id,
-                            TargetTableId = targetTableId,
-                            SourceTableId = sourceTableId,
-                            Columns = new List<Models.ConstraintColumnPair>
-                            {
-                                new()
-                                {
-                                    TargetColumnId = targetColumn,
-                                    SourceColumnId = sourceColumn,
-                                }
-                            },
-                        });
-                    }
-                }
-            }
-
-            return new()
-            {
-                Tables = tables,
-                Columns = columns,
-                Constraints = constraints,
-            };
+            return internalFactory.Create();
         }
     }
 }
