64cbda402bf5436d1774b4880fdb010290280ba1
Nick Emmerson <nickemmerson619@hotmail.com>
2023-03-19 23:06:07 +0000

    Have a go at making a Context singleton class

---
+++ sql-helper/SqlHelper/Contexts/IContextFactory.cs
@@ -0,0 +1,59 @@
+﻿using SqlHelper.Config;
+using SqlHelper.Helpers;
+
+namespace SqlHelper.Contexts
+{
+    public interface IContextFactory
+    {
+        public IUniqueIdProvider CreateUniqueIdProvider();
+
+        public IConfigManager CreateConfigManager();
+    }
+
+    public class StandardContextFactory: IContextFactory
+    {
+        public IUniqueIdProvider CreateUniqueIdProvider() => new SequentialUniqueIdProvider();
+
+        public IConfigManager CreateConfigManager() => new AppResourceConfigManager(new FileManager());
+    }
+
+    public static class Context
+    {
+        private static IContextFactory _contextFactory;
+
+        private static IUniqueIdProvider _uniqueIdProvider;
+        private static IConfigManager _configManager;
+
+        static Context()
+        {
+            _contextFactory = new StandardContextFactory();
+        }
+
+        public static void Use(IContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+            _uniqueIdProvider = null;
+            _configManager = null;
+        }
+
+        public static IUniqueIdProvider UniqueId
+        {
+            get
+            {
+                if (_uniqueIdProvider is null)
+                    _uniqueIdProvider = _contextFactory.CreateUniqueIdProvider();
+                return _uniqueIdProvider;
+            }
+        }
+
+        public static IConfigManager Config
+        {
+            get
+            {
+                if (_configManager is null)
+                    _configManager = _contextFactory.CreateConfigManager();
+                return _configManager;
+            }
+        }
+    }
+}
---
+++ sql-helper/SqlHelper/Helpers/IUniqueIdProvider.cs
@@ -0,0 +1,25 @@
+﻿namespace SqlHelper.Helpers
+{
+    public interface IUniqueIdProvider
+    {
+        public long Next();
+    }
+
+    public class SequentialUniqueIdProvider : IUniqueIdProvider
+    {
+        private long _next;
+
+        public SequentialUniqueIdProvider(long start = 0L)
+        {
+            _next = start;
+        }
+
+        public long Next()
+        {
+            var next = _next++;
+            return next;
+        }
+    }
+
+
+}
