61cf97c5f438ddd9f8a0a4d622be329bd1228c06
Nick Emmerson <nickemmerson619@hotmail.com>
2023-04-02 18:06:47 +0100

    Add test coverage for AppResourceConfigManager

--- sql-helper/SqlHelper.Test/Config/AppResourceConfigManagerTests.cs
+++ sql-helper/SqlHelper.Test/Config/AppResourceConfigManagerTests.cs
@@ -2,6 +2,9 @@
 using FluentAssertions;
 using SqlHelper.Config;
 using SqlHelper.Helpers;
+using SqlHelper.Models;
+using System.Text.Json;
+using System.Text.RegularExpressions;
 using Xunit;
 
 namespace SqlHelper.Test.Config
@@ -52,7 +55,6 @@ namespace SqlHelper.Test.Config
         public void List_ShouldReturnFilesInTopDirectoryOnly()
         {
             // ARRANGE
-            var wd = Directory.GetCurrentDirectory();
             var files = new List<string>
             {
                 @"C:\test\test1.json",
@@ -75,6 +77,58 @@ namespace SqlHelper.Test.Config
 
             // ASSERT
             actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Read_ShouldReadDbDataForExistingConfig()
+        {
+            // ARRANGE
+            var alias = "test";
+            var dbDataContent = File.ReadAllText("./TestData/DbData.json");
+            A.CallTo(() => _mockFileManager.Read(@"C:\test\test.json"))
+                .Returns((true, dbDataContent));
+
+            // ACT
+            (var actual_exists, var actual_dbData) = _configManager.Read(alias);
+
+            // ASSERT
+            actual_exists.Should().BeTrue();
+            actual_dbData?.Tables.Should().NotBeNull();
+            actual_dbData?.Columns.Should().NotBeNull();
+            actual_dbData?.Constraints.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Read_ShouldDetectNonExistingConfig()
+        {
+            // ARRANGE
+            var alias = "test";
+            A.CallTo(() => _mockFileManager.Read(@"C:\test\test.json"))
+                .Returns((false, null));
+
+            // ACT
+            (var actual_exists, var actual_dbData) = _configManager.Read(alias);
+
+            // ASSERT
+            actual_exists.Should().BeFalse();
+            actual_dbData.Should().BeNull();
+        }
+
+        [Fact]
+        public void Write_ShouldWriteDataToFile()
+        {
+            // ARRANGE
+            var alias = "test";
+            var dbDataContent = File.ReadAllText("./TestData/DbData.json");
+            dbDataContent = Regex.Replace(dbDataContent, "\\s", "");
+            var dbData = JsonSerializer.Deserialize<DbData>(dbDataContent);
+
+            // ACT
+            _configManager.Write(alias, dbData);
+
+            // ASSERT
+            A.CallTo(() => _mockFileManager.Write(@"C:\test\test.json", dbDataContent))
+                .MustHaveHappened();
         }
     }
 }
